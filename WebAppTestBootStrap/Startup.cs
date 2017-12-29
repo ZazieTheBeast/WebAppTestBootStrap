ba226562e7f497e4566e3c80255108acb90c930e
Kristofer Kass <zazism_rx@live.com>
2017-12-29 10:05:35 -0500

    changing to another blog

--- WebAppTestBootStrap/Startup.cs
+++ WebAppTestBootStrap/Startup.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Ninject;
-using Ninject.Web.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
